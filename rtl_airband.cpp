cf52bbb59f0ee70f8440a678ddcf7f93ca843177
Tomasz Lemiech <szpajder@gmail.com>
2016-01-23 15:07:42 +0100

    Bump version to 2.0.1

--- rtl_airband.cpp
+++ rtl_airband.cpp
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define RTL_AIRBAND_VERSION "2.0.0"
+#define RTL_AIRBAND_VERSION "2.0.1"
 #if defined USE_BCM_VC && !defined __arm__
 #error Broadcom VideoCore support can only be enabled on ARM builds
 #endif

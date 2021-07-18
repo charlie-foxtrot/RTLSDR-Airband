a33f89ce0688e80a28fd39eb96d1a645c929d73a
Tomasz Lemiech <szpajder@gmail.com>
2021-07-18 11:07:47 +0200

    Upscale I/Q samples before writing them to a file
    2d412580 changed the scaling factor of the input samples and now
    we are operating internally on a range between -1.0 to 1.0[1]. When
    using rawfile outputs, samples must be upscaled back.
    
    [1] In fact, samples after FFT and decimation may exceed this value
    due to the fact that the channel of interest may not necessarily
    be exactly centered on the configured frequency. If this is the case,
    I/Q samples written into rawfile outputs are superimposed onto a
    complex exponential and this stretches the range of their values.
    In order not to overflow the CS16 range, samples are multiplied by
    1024.0 rather than 32768, just to be on a safe side.

--- src/output.cpp
+++ src/output.cpp
@@ -491,7 +491,7 @@ void process_outputs(channel_t *channel, int cur_scan_freq) {
 			} else if(channel->outputs[k].type == O_RAWFILE) {
 				int16_t iq_buf[2 * WAVE_BATCH];
 				buflen = 2 * WAVE_BATCH * sizeof(int16_t);
-				for(size_t k = 0; k < 2 * WAVE_BATCH; iq_buf[k] = (int16_t)(channel->iq_out[k]), k++)
+				for(size_t k = 0; k < 2 * WAVE_BATCH; iq_buf[k] = (int16_t)(1024.0f * channel->iq_out[k]), k++)
 					;
 				written = fwrite(iq_buf, 1, buflen, fdata->f);
 			}

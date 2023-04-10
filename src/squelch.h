6e6df25e08813a098a8cb9a6760fb5878ba0ba93
charlie-foxtrot <13514783+charlie-foxtrot@users.noreply.github.com>
2023-04-10 14:29:47 -0700

    fixup

--- src/squelch.h
+++ src/squelch.h
@@ -1,7 +1,26 @@
+/*
+ * squelch.h
+ *
+ * Copyright (C) 2022-2023 charlie-foxtrot
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _SQUELCH_H
 #define _SQUELCH_H
 
-#include <cstddef> // needed for size_t
+#include <cstddef>  // size_t
 
 #ifdef DEBUG_SQUELCH
 #include <stdio.h>  // needed for debug file output

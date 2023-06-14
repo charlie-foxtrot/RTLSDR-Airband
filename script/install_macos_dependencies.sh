64551daa2f6fc9981b527154dfd15281e960ba7a
Sergio Vavassori <vavassori@gmail.com>
2023-06-15 00:28:33 +0200

    Adds new package workflow with separate scripts

---
+++ script/install_macos_dependencies.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+brew update
+brew upgrade
+brew install \
+    lame \
+    libshout \
+    libconfig \
+    fftw \
+    librtlsdr \
+    soapysdr \
+    pulseaudio \
+    pkg-config
